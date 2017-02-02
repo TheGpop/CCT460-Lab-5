7aeaeeb2177062dc7eded5b591a375cbd6a03be5
TheGpop <gianmarco.castro@mail.utoronto.ca>
2017-02-02 16:32:52 -0500

    moved files out of folder so repository goes straight to files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Boku_no_Kawaii
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'boku-no-kawaii' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'boku-no-kawaii' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'boku-no-kawaii' ), 'boku-no-kawaii', '<a href="https://automattic.com/" rel="designer">Sachiko</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
